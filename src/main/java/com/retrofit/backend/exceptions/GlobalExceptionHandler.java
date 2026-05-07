a7667d2018edb44a206b68745310cf7d09650114
espinozaje <jeisson12aaron@gmail.com>
2026-05-07 16:13:16 -0500

    manejo de excepciones

--- src/main/java/com/retrofit/backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/retrofit/backend/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.retrofit.backend.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -30,6 +31,16 @@ public class GlobalExceptionHandler {
         body.put("error", "Bad Request");
         body.put("message", ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleBadCredentialsException(BadCredentialsException ex, WebRequest request) {
+        Map<String, String> body = new HashMap<>();
+        body.put("status", "401");
+        body.put("error", "Unauthorized");
+        body.put("message", "Usuario o contraseña incorrectos");
+        return new ResponseEntity<>(body, HttpStatus.UNAUTHORIZED);
     }
 
 }
