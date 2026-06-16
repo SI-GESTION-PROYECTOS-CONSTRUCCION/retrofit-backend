e57915ad54dff204faf3d9061b038895575f3921
espinozaje <jeisson12aaron@gmail.com>
2026-06-16 16:02:58 -0500

    fix: update ProjectItem ID generation strategy to IDENTITY

--- src/main/java/com/retrofit/backend/model/ProjectItem.java
+++ src/main/java/com/retrofit/backend/model/ProjectItem.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Data
 public class ProjectItem {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
